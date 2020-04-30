5e39e7f5917e2afaa2a1365ba3caa79158d07ed6
Phil Hawksworth <phil@netlify.com>
2020-04-30 12:44:14 +0100

    disokay returned images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
   return {
     dir: {
       input: "src",
